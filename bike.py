27f1d45dc4ab4b25ee95e75c79fb66725c4363f0
Alexander Zsikla <alesandor00@gmail.com>
2019-10-11 22:16:13 -0400

    bike class

---
+++ bike.py
@@ -0,0 +1,24 @@
+'''
+Definitions of a bike, which has an ID number, who the last user of the bike
+was, when the bike was checked out (if it was taken out), and whether or not
+it needs maintaince
+'''
+
+class Bike:
+    
+    ID = 0
+    last_user = ""
+    checkout_time = 0
+    needs_maintaince = False
+    
+    def __init__(self, ID):
+        self.ID = ID
+        self.checkout_time = -1
+        
+    def update_time(self, time):
+        self.checkout_time = time
+
+    def broken(self):
+        self.maintaince = True
+
+    
