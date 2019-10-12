09638db6ca5cda0f28f753cf3dd7b1ab9563217a
Alexander Zsikla <alesandor00@gmail.com>
2019-10-11 23:10:59 -0400

    added documentation

--- send_info.py
+++ send_info.py
@@ -8,6 +8,16 @@ import json
 
 app = Flask(__name__)
 
+'''
+create_list
+Input: a bike
+Output: a list of the bike's information
+Does: creates a list and returns it
+list[0] --> bike ID
+list[1] --> last user
+list[2] --> checkout_time
+list[3] --> needs_maintenance
+'''
 def create_list(b):
     list = [b.ID, 
             b.last_user, 
